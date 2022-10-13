4fb4afabb1d3b654ed1892232704f4e4a233f186
Yousef497 <yousef.shalaby70@gmail.com>
2022-10-14 00:15:06 +0200

    Updated

--- GenerateTextFile.py
+++ GenerateTextFile.py
@@ -1,17 +1,19 @@
 import random
 import time
+import math
 
 hexa = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
 
-numOfChar = int((float(input("Please input size of generated hexa text file in GB: ")) * 1000000000))
+name = input("Please input the Generated File Name: ")
+numOfChar = int((float(input("Please input size of generated hexa text file in GB: ")) * pow(2, 30)))
 #print(numOfChar)
 
 start = time.process_time()
-with open("input.txt", 'w') as file:
+with open(name, 'w') as file:
     # i is counter for num of characters start from zero to end
     for i in range(0, numOfChar):
         val = random.randint(0, 15)
         file.write(hexa[val])
 
 print("The File is ready")   
-print("Time taken is: " + time.process_time() - start)
+print("Time taken is: " + str(time.process_time() - start))
